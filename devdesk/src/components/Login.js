cb417807e75ca9c0454b459fb7746dbdf3db8e0c
angelynaliem <angelynaliem@gmail.com>
2020-08-19 22:39:06 -0400

    Added Form in Login

--- devdesk/src/components/Login.js
+++ devdesk/src/components/Login.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-const Login = () => {
+const Login = (props) => {
+
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+
+        <Form inline>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Label htmlFor="username" className="mr-sm-2">Username</Label>
+          <Input type="text" name="username" id="username" placeholder="Austin Allred" />
+        </FormGroup>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Label htmlFor="password" className="mr-sm-2">Password</Label>
+          <Input type="password" name="password" id="password" placeholder="Don't tell!" />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+      
+    );
+    
 }
 
 export default Login;
