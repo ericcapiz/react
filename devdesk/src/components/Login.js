89f382f075458af8066ca5030eb0d84664268355
angelynaliem <angelynaliem@gmail.com>
2020-08-19 23:05:58 -0400

    Deleted body content in App.js

--- devdesk/src/components/Login.js
+++ devdesk/src/components/Login.js
@@ -6,17 +6,21 @@ const Login = (props) => {
     return (
 
         <Form inline>
+
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label htmlFor="username" className="mr-sm-2">Username</Label>
           <Input type="text" name="username" id="username" placeholder="Austin Allred" />
         </FormGroup>
+        
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label htmlFor="password" className="mr-sm-2">Password</Label>
           <Input type="password" name="password" id="password" placeholder="Don't tell!" />
         </FormGroup>
+        
         <Button>Submit</Button>
-      </Form>
       
+        </Form>
+
     );
     
 }
