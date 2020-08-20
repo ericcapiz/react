1723b94c111771c467963f85034522d0f6122622
angelynaliem <angelynaliem@gmail.com>
2020-08-19 23:15:06 -0400

    Created handleChanges function

--- devdesk/src/components/Login.js
+++ devdesk/src/components/Login.js
@@ -3,18 +3,36 @@ import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const Login = (props) => {
 
+    const [user, setUser] = useState({ Username: "", Password: "" });
+
+    const handleChanges = (e) => {
+
+        const newUser = { ...user, [e.target.name]: e.target.value };
+        setUser(newUser);
+      };
+
     return (
 
         <Form inline>
 
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label htmlFor="username" className="mr-sm-2">Username</Label>
-          <Input type="text" name="username" id="username" placeholder="Austin Allred" />
+          <Input 
+          type="text" 
+          name="username" 
+          id="username" 
+          placeholder="Austin Allred" 
+          onChange={handleChanges} />
         </FormGroup>
         
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label htmlFor="password" className="mr-sm-2">Password</Label>
-          <Input type="password" name="password" id="password" placeholder="Don't tell!" />
+          <Input 
+          type="password" 
+          name="password" 
+          id="password" 
+          placeholder="Don't tell!"
+          onChange={handleChanges} />
         </FormGroup>
         
         <Button>Submit</Button>
