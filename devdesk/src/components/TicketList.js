0ea686a8a13a6df3408276856d7972f22e322e78
angelynaliem <angelynaliem@gmail.com>
2020-08-21 00:11:28 -0400

    Added dummy data in ticketlist

--- devdesk/src/components/TicketList.js
+++ devdesk/src/components/TicketList.js
@@ -3,8 +3,34 @@ import React from 'react'
 const TicketList = () => {
 
     return (
+        <div>
 
-        <h5>Ticket 1</h5>
+            <div>
+        <h5>Ticket Number (1)</h5>
+        <h5>Category (React)</h5>
+        <h6>Title: (Issue with setting up state)</h6>
+        <h6>Description: (I can't get my state to render)</h6>
+        <h6>What I've tried: (I tried to use curly brackets)</h6>
+            </div>
+
+            <div>
+        <h5>Ticket Number (2)</h5>
+        <h5>Category (Back End)</h5>
+        <h6>Title: (How to set up API?)</h6>
+        <h6>Description: (I don't know where to start!)</h6>
+        <h6>What I've tried: (Set up Zoom call with Nate to get some help)</h6>
+            </div>
+
+            <div>
+        <h5>Ticket Number (3)</h5>
+        <h5>Category (SASS)</h5>
+        <h6>Title: (Problem importing reactstrap with SASS)</h6>
+        <h6>Description: (I am not sure how to use both SASS and Reactstrap together)</h6>
+        <h6>What I've tried: (I tried Import the Reactstrap component)</h6>
+            </div>
+
+        </div>
+
 
 
     )
