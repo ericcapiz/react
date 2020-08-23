b7a6e267ef48588eddfcc4ab8e61931201edb1cc
eric capiz <ericcapiz@gmail.com>
2020-08-23 12:37:25 -0500

    helper dash completed

--- devdesk/src/components/Helper/HelperBody.js
+++ devdesk/src/components/Helper/HelperBody.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import phone from './phone.png';
 
 const MainDiv =styled.div `
     display: flex;
@@ -10,12 +11,23 @@ const SideDiv = styled.div `
 display: flex;
 flex-direction: column;
 margin-top: 150px;
+button{
+    color:black;
+    border-radius:10px;
+    background-color: #48c8e8;
+    height: 50px;
+    font-size:20px;
+}
 `
 const SideDiv2 = styled.div `
 display: flex;
-background-color: blue;
 height: 500px;
 width: 100vh;
+
+img{
+    object-fit:scale-down;
+    // height: 500px;
+}
 `
 
 const HelperBody = ()=>{
@@ -26,7 +38,7 @@ const HelperBody = ()=>{
         <p>View student help requests and assign tickets to yourself and others</p>
         <button>Get Started</button>
         </SideDiv>
-        <SideDiv2><h1>imag place holder on this side</h1></SideDiv2>
+        <SideDiv2><img src={phone}></img></SideDiv2>
         </MainDiv>
     )
 
