8ddb430af158a6f26bcfd88f3a098ea687f0f78b
Tim Shaker <tim@tmshkr.com>
2020-08-27 17:55:49 -0700

    fix props.tickets.map is undefined error

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,40 +1,36 @@
-import React, { useEffect } /*{ useState, useEffect }*/ from "react";
+import React, { useEffect /*{ useState, useEffect }*/ } from "react";
 //import dummyData from "./students/dummyData";
 import Ticket from "./students/Ticket";
 //import { axiosAuth } from "./utils/axiosAuth";
 //import { useParams } from "react-router-dom";
-import { connect } from 'react-redux';
-import { getTicketData } from './actions/actions'
+import { connect } from "react-redux";
+import { getTicketData } from "./actions/actions";
 
-const TicketList = props => {
-  console.log('ticket', props)
+const TicketList = (props) => {
+  console.log("ticket", props);
 
   useEffect(() => {
-    props.getTicketData()
-    console.log(props.tickets)
-  }, [])
+    props.getTicketData();
+    console.log(props.tickets);
+  }, []);
 
+  if (!Array.isArray(props.tickets)) return <></>;
   return (
-    
     <>
-      {props.tickets.map(ticket => {
-        return <Ticket key={ticket.ticket_id} tickets={ticket} />
-
+      {props.tickets.map((ticket) => {
+        return <Ticket key={ticket.ticket_id} tickets={ticket} />;
       })}
     </>
   );
 };
 
-const mapStateToProps = state => {
-  return {    
+const mapStateToProps = (state) => {
+  return {
     tickets: state.tickets,
     user: state.user,
     isFetching: state.isFetching,
-    error: state.error
-  }
-}
+    error: state.error,
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  {getTicketData}
-  )(TicketList)
+export default connect(mapStateToProps, { getTicketData })(TicketList);
