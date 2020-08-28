d9bc5ae034db3670d8e58b7f67bbb1ae7ca87381
Tim Shaker <tim@tmshkr.com>
2020-08-27 18:05:23 -0700

    update addNewTicket

--- src/components/actions/actions.js
+++ src/components/actions/actions.js
@@ -1,40 +1,39 @@
-import { axiosAuth } from '../utils/axiosAuth'
+import { axiosAuth } from "../utils/axiosAuth";
 
-export const SET_GETTING_TICKET_DATA = 'SET_GETTING_TICKET_DATA'
-export const SET_TICKET_DATA = 'SET_TICKET_DATA'
-export const SET_TICKET_ERROR = 'SET_TICKET_ERROR'
-export const ADD_NEW_TICKET = 'ADD_NEW_TICKET'
-export const SET_NEW_TICKET = 'SET_NEW_TICKET'
+export const SET_GETTING_TICKET_DATA = "SET_GETTING_TICKET_DATA";
+export const SET_TICKET_DATA = "SET_TICKET_DATA";
+export const SET_TICKET_ERROR = "SET_TICKET_ERROR";
+export const ADD_NEW_TICKET = "ADD_NEW_TICKET";
+export const SET_NEW_TICKET = "SET_NEW_TICKET";
 
-export const getTicketData = () => dispatch => {
-    console.log('data', dispatch)
-    dispatch({ type: SET_GETTING_TICKET_DATA })
-    axiosAuth()
-    .get('/api/tickets')
-    .then(res => {
-        console.log('api', res.data)
-        dispatch({ type: SET_TICKET_DATA, payload: res.data })
+export const getTicketData = () => (dispatch) => {
+  console.log("data", dispatch);
+  dispatch({ type: SET_GETTING_TICKET_DATA });
+  axiosAuth()
+    .get("/api/tickets")
+    .then((res) => {
+      console.log("api", res.data);
+      dispatch({ type: SET_TICKET_DATA, payload: res.data });
+    })
+    .catch((err) => {
+      console.log("get API failed", err.message);
+      dispatch({ type: SET_TICKET_ERROR, payload: err.message });
+    });
+};
 
+export const addNewTicket = (tickets) => (dispatch) => {
+  console.log("add", dispatch);
+  dispatch({ type: ADD_NEW_TICKET });
+  axiosAuth()
+    .post("/api/tickets", tickets)
+    .then((res) => {
+      console.log("api", res.data);
+      // dispatch({ type: SET_NEW_TICKET, payload: res.data})
     })
-    .catch(err => {
-        console.log('get API failed', err.message)
-        dispatch({ type: SET_TICKET_ERROR, payload: err.message })
-    })
-}
-
-export const addNewTicket = (tickets) => dispatch => {
-    console.log('add', dispatch)
-    dispatch({ type: ADD_NEW_TICKET })
-    axiosAuth()
-    .post('/api/tickets', tickets)
-    .then(res => {
-        console.log('api', res.data)
-        dispatch({ type: SET_NEW_TICKET, payload: res.data})
-    })
-    .catch(err => {
-        console.log('API failed', err.message)
-    })
-}
+    .catch((err) => {
+      console.log("API failed", err.message);
+    });
+};
 
 // export const login = (user) => dispatch => {
 //     console.log('login', dispatch)
